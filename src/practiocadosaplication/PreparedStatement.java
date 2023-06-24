4f4cb98d528b2405547441fd058a9e29e9905fa9
Rxicay <rxicay78@gmail.com>
2023-06-23 19:08:51 -0600

    se agregta el proyecto

---
+++ src/practiocadosaplication/PreparedStatement.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practiocadosaplication;
+
+/**
+ *
+ * @author Rodolfo
+ */
+class PreparedStatement {
+    
+}
